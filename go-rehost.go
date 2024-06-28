abe3deacc9cff674d6494f76e6f4db0b9a25fea5
Flaie <90042274+flaiehfr@users.noreply.github.com>
2024-06-28 08:42:22 +0200

    Update go-rehost.go

--- go-rehost.go
+++ go-rehost.go
@@ -10,10 +10,10 @@ import (
     "os"
     "path/filepath"
     "strings"
-	  "math/rand"
-	  "encoding/json"
-	  "time"
-	  "strconv"
+    "math/rand"
+    "encoding/json"
+    "time"
+    "strconv"
 )
 
 type UploadResponse struct {
@@ -21,7 +21,9 @@ type UploadResponse struct {
 }
 
 func main() {
+    // Define and parse command-line arguments
     filePath := flag.String("file", "", "Path to the file to upload")
+    cookieVal := flag.String("cookie", "", "Rehost cookie value")
     flag.Parse()
 
     if *filePath == "" {
@@ -29,40 +31,55 @@ func main() {
         return
     }
 
-    err := uploadFile(*filePath)
+    var cookie string
+    if *cookieVal != "" {
+        cookie = ".AspNet.ApplicationCookie=" + *cookieVal
+    }
+    fmt.Println(cookie)
+
+    err := uploadFile(*filePath, cookie)
     if err != nil {
-        fmt.Println("Error uploading file: ", err)
+        fmt.Println("Error uploading file:", err)
     } 
 }
 
-func uploadFile(filePath string) error {
+func uploadFile(filePath string, cookie string) error {
+    // Open the file
     file, err := os.Open(filePath)
     if err != nil {
-        return fmt.Errorf("Could not open file: %w", err)
+        return fmt.Errorf("could not open file: %w", err)
     }
     defer file.Close()
 
+    // Create a buffer to store the file and form data
     var requestBody bytes.Buffer
     writer := multipart.NewWriter(&requestBody)
 
-    part, err := writer.CreateFormFile("_" + randomString(9) + ".png", filepath.Base(filePath))
+    // Create a form file field
+    part, err := writer.CreateFormFile("_"+randomString(9)+".png", filepath.Base(filePath))
     if err != nil {
-        return fmt.Errorf("Could not create form file: %w", err)
+        return fmt.Errorf("could not create form file: %w", err)
     }
 
+    // Copy the file content to the form file field
     _, err = io.Copy(part, file)
     if err != nil {
-        return fmt.Errorf("Could not copy file content: %w", err)
+        return fmt.Errorf("could not copy file content: %w", err)
     }
+
+    // Close the writer to finalize the form data
     writer.Close()
 
+    // Create the HTTP request
     url := "https://rehost.diberie.com/Host/UploadFiles?PrivateMode=false&SendMail=false&Comment="
     req, err := http.NewRequest("POST", url, &requestBody)
     if err != nil {
         return fmt.Errorf("could not create HTTP request: %w", err)
     }
     req.Header.Set("Content-Type", writer.FormDataContentType())
+    req.Header.Set("Cookie", cookie)
 
+    // Perform the HTTP request
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
@@ -70,26 +87,31 @@ func uploadFile(filePath string) error {
     }
     defer resp.Body.Close()
 
+    // Check for HTTP response status
     if resp.StatusCode != http.StatusOK {
         return fmt.Errorf("unexpected response status: %s", resp.Status)
     }
 
+    // Read and parse the response body
     responseBody, err := io.ReadAll(resp.Body)
     if err != nil {
         return fmt.Errorf("could not read response body: %w", err)
     }
 
+    // Unmarshal the JSON response
     var uploadResponse UploadResponse
     err = json.Unmarshal(responseBody, &uploadResponse)
     if err != nil {
         return fmt.Errorf("could not parse JSON response: %w", err)
     }
 
+    // Output the constructed URL
     fmt.Println("https://rehost.diberie.com/Picture/Get/f/" + strconv.FormatInt(uploadResponse.PicID, 10))
 
     return nil
 }
 
+// Helper function to generate a random string
 func randomString(length int) string {
     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     rand.Seed(time.Now().UnixNano())
